382c242283024a033a93376de1c5d27f5bc34531
Rory McKinley <rorymckinley@gmail.com>
2011-11-29 21:37:07 +0200

    Convert snippets with Ruby1.9 keys to symbols

--- plugin/rubyhash.vim
+++ plugin/rubyhash.vim
@@ -1,4 +1,4 @@
 function! ToSymbolKeysLinewise()
   .s/['"]\(\w\+\)['"]\(\s*=>\)/:\1\2/g
-  .s/\v(\{|,)\s*\zs(\w+):/:\2 =>/g
+  .s/\v(\{|,|^)\s*\zs(\w+):/:\2 =>/g
 endfunction
--- spec/plugin/rubyhash_spec.rb
+++ spec/plugin/rubyhash_spec.rb
@@ -24,4 +24,23 @@ describe "rubyhash plugin" do
       result.body.should == "{:key_one =>'value_one', :keytwo => 'value:two', :keythree => 'value3', :key4 => My::Class}"
     end
   end
+
+  describe "operating on a single line containing a snippet of a hash" do
+    it "converts string hash keys to symbol hash keys" do
+      input_buffer = "        'my_key'=>'my_value', \"new_key\"=>'new_value', 'final_key' => 'final_value'"
+      @commands += [ ":call ToSymbolKeysLinewise()"]
+
+      result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
+      result.body.should ==  "        :my_key=>'my_value', :new_key=>'new_value', :final_key => 'final_value'"
+    end
+
+    it "converts Ruby 1.9 style keys to symbols" do
+      input_buffer = "   key_one:'value_one', keytwo: 'value:two', keythree: 'value3', key4: My::Class"
+      @commands += [ ":call ToSymbolKeysLinewise()"]
+
+      result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
+      result.body.should == "   :key_one =>'value_one', :keytwo => 'value:two', :keythree => 'value3', :key4 => My::Class"
+    end
+  end
+
 end
