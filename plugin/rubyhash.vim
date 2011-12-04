b738301111f0d03f8ca5dec97c57c11efa058ebc
Rory McKinley <rorymckinley@gmail.com>
2011-12-04 21:26:59 +0200

    Converting symbols to Ruby1.9

--- plugin/rubyhash.vim
+++ plugin/rubyhash.vim
@@ -2,3 +2,7 @@ function! ToSymbolKeysLinewise()
   .s/['"]\(\w\+\)['"]\(\s*=>\)/:\1\2/g
   .s/\v(\{|,|^)\s*\zs(\w+):/:\2 =>/g
 endfunction
+
+function! To19KeysLinewise()
+  .s/\v:(\w+)\s*\=\>/\1:/g
+endfunction
--- spec/plugin/rubyhash_spec.rb
+++ spec/plugin/rubyhash_spec.rb
@@ -8,54 +8,78 @@ describe "rubyhash plugin" do
   end
 
   describe "operating on a single line containing a complete hash" do
-    it "converts string hash keys to symbol hash keys" do
-      input_buffer = "{'my_key'=>'my_value', \"new_key\"=>'new_value', 'final_key' => 'final_value'}"
-      @commands += [ ":call ToSymbolKeysLinewise()"]
+    describe "converting to symbol keys" do
+      it "converts string hash keys to symbol hash keys" do
+        input_buffer = "{'my_key'=>'my_value', \"new_key\"=>'new_value', 'final_key' => 'final_value'}"
+        @commands += [ ":call ToSymbolKeysLinewise()"]
 
-      result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
-      result.body.should == "{:my_key=>'my_value', :new_key=>'new_value', :final_key => 'final_value'}"
+        result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
+        result.body.should == "{:my_key=>'my_value', :new_key=>'new_value', :final_key => 'final_value'}"
+      end
+
+      it "converts Ruby 1.9 style keys to symbols" do
+        input_buffer = "{key_one:'value_one', keytwo: 'value:two', keythree: 'value3', key4: My::Class}"
+        @commands += [ ":call ToSymbolKeysLinewise()"]
+
+        result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
+        result.body.should == "{:key_one =>'value_one', :keytwo => 'value:two', :keythree => 'value3', :key4 => My::Class}"
+      end
+
+      it "leaves existing symbol keys untouched when converting to symbol keys" do
+        input_buffer = "{ change_me: 'ping', :leave_me => 'pang', 'change_me_too' => 'pong' }"
+        @commands += [ ":call ToSymbolKeysLinewise()"]
+
+        result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
+        result.body.should == "{ :change_me => 'ping', :leave_me => 'pang', :change_me_too => 'pong' }"
+      end
     end
 
-    it "converts Ruby 1.9 style keys to symbols" do
-      input_buffer = "{key_one:'value_one', keytwo: 'value:two', keythree: 'value3', key4: My::Class}"
-      @commands += [ ":call ToSymbolKeysLinewise()"]
+    describe "converting to ruby1.9 keys" do
+      it "converts symbol keys" do
+        input_buffer = %q{{:key_one=>'val_one', :key_two => "val_two"}}
+        @commands += [ ":call To19KeysLinewise()"]
 
-      result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
-      result.body.should == "{:key_one =>'value_one', :keytwo => 'value:two', :keythree => 'value3', :key4 => My::Class}"
-    end
-
-    it "leaves existing symbol keys untouched when converting to symbol keys" do
-      input_buffer = "{ change_me: 'ping', :leave_me => 'pang', 'change_me_too' => 'pong' }"
-      @commands += [ ":call ToSymbolKeysLinewise()"]
-
-      result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
-      result.body.should == "{ :change_me => 'ping', :leave_me => 'pang', :change_me_too => 'pong' }"
+        result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
+        result.body.should == %q|{key_one:'val_one', key_two: "val_two"}|
+      end
     end
   end
 
   describe "operating on a single line containing a snippet of a hash" do
-    it "converts string hash keys to symbol hash keys" do
-      input_buffer = "        'my_key'=>'my_value', \"new_key\"=>'new_value', 'final_key' => 'final_value'"
-      @commands += [ ":call ToSymbolKeysLinewise()"]
+    describe "converting to symbol keys" do
+      it "converts string hash keys to symbol hash keys" do
+        input_buffer = "        'my_key'=>'my_value', \"new_key\"=>'new_value', 'final_key' => 'final_value'"
+        @commands += [ ":call ToSymbolKeysLinewise()"]
 
-      result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
-      result.body.should ==  "        :my_key=>'my_value', :new_key=>'new_value', :final_key => 'final_value'"
+        result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
+        result.body.should ==  "        :my_key=>'my_value', :new_key=>'new_value', :final_key => 'final_value'"
+      end
+
+      it "converts Ruby 1.9 style keys to symbols" do
+        input_buffer = "   key_one:'value_one', keytwo: 'value:two', keythree: 'value3', key4: My::Class"
+        @commands += [ ":call ToSymbolKeysLinewise()"]
+
+        result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
+        result.body.should == "   :key_one =>'value_one', :keytwo => 'value:two', :keythree => 'value3', :key4 => My::Class"
+      end
+
+      it "leaves existing symbol keys untouched when converting to symbol keys" do
+        input_buffer = " change_me: 'ping', :leave_me => 'pang', 'change_me_too' => 'pong' "
+        @commands += [ ":call ToSymbolKeysLinewise()"]
+
+        result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
+        result.body.should == " :change_me => 'ping', :leave_me => 'pang', :change_me_too => 'pong' "
+      end
     end
 
-    it "converts Ruby 1.9 style keys to symbols" do
-      input_buffer = "   key_one:'value_one', keytwo: 'value:two', keythree: 'value3', key4: My::Class"
-      @commands += [ ":call ToSymbolKeysLinewise()"]
+    describe "converting to ruby1.9 keys" do
+      it "converts symbol keys" do
+        input_buffer = %q|:key_one=>'val_one', :key_two => "val_two"|
+        @commands += [ ":call To19KeysLinewise()"]
 
-      result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
-      result.body.should == "   :key_one =>'value_one', :keytwo => 'value:two', :keythree => 'value3', :key4 => My::Class"
-    end
-
-    it "leaves existing symbol keys untouched when converting to symbol keys" do
-      input_buffer = " change_me: 'ping', :leave_me => 'pang', 'change_me_too' => 'pong' "
-      @commands += [ ":call ToSymbolKeysLinewise()"]
-
-      result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
-      result.body.should == " :change_me => 'ping', :leave_me => 'pang', :change_me_too => 'pong' "
+        result = @runner.run(:commands => @commands.join("\n")+"\n", :input_file => input_buffer)
+        result.body.should == %q|key_one:'val_one', key_two: "val_two"|
+      end
     end
   end
 
